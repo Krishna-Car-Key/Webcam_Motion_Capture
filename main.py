28db266b0967708565fd117a09f62d1694a10bc1
krishna <krishnakarki1996@gmail.com>
2024-05-17 14:22:47 +0545

    convert frame to gray_gaussianBlur, store first frame

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ video = cv2.VideoCapture(0)
 # sleep for 1sec to let the camera load
 time.sleep(1)
 
+first_frame = None
 while True:
     check, frame = video.read()
+
+    # convert frame into gray_sframe and blur them
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (25, 25), 0)
+
+    # caught first frame here
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
     cv2.imshow("Video", frame)
 
     key = cv2.waitKey(0)
