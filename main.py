08c1e178356bcebd7be7113c8980dea5157ae9c4
krishna <krishnakarki1996@gmail.com>
2024-05-17 14:29:11 +0545

    use contours to create rectangle on frame

--- main.py
+++ main.py
@@ -23,6 +23,14 @@ while True:
     thresh_frame = cv2.threshold(delta_frame, 65, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
     cv2.imshow("Video", frame)
 
     key = cv2.waitKey(0)
