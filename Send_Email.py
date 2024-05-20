4905cf8d8396f8429df953cca7d135e97eae7ebd
krishna <krishnakarki1996@gmail.com>
2024-05-20 22:59:57 +0545

    modify email

--- Send_Email.py
+++ Send_Email.py
@@ -1,2 +1,19 @@
+import os
+import smtplib
+import ssl
+
+HOST = "smtp.gmail.com"
+CONTEXT = ssl.create_default_context()
+password = os.getenv("PASSWORD")
+PORT = 465
+
+
 def send_email():
-    print("Email was sent!")
+    message = ("""\
+Subject: Some object caught\n
+There is some object front of me!
+""")
+    username = "emailexperimental70@gmail.com"
+    with smtplib.SMTP_SSL(HOST, PORT, context=CONTEXT) as server:
+        server.login(username, password=password)
+        server.sendmail(username, username, message)
